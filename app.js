0aabeede9a199b375c9d5d83f6b54edc5891338e
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-13 12:46:39 -0300

    Server created

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /* First Command */
 //console.log('Hello Node!')
 
+/* Second Command */
+var http = require('http')
+
+var server = http.createServer(function(req, res){
+    var page = req.url
+    console.log(page)
+})
+
+console.log('localhost:8000')
+server.listen(8000)
