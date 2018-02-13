57366586cc7a794c550f16e047e487e743e5c06e
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-13 15:45:05 -0300

    Lesson 1 - Chapter 13: Model

---
+++ app/models/clientModel.js
@@ -0,0 +1,9 @@
+module.exports = function(){
+    this.all = function(){
+        return [
+            {name:'Paulo', email:'paulo@email.com'}
+        ]
+    }
+
+    return this
+}
--- app/routes/web.js
+++ app/routes/web.js
@@ -1,7 +1,12 @@
+/* Lesson 1 - Chapter 13: Models */
+var clientModel = require('../models/clientModel')()
+
 /* Lesson 1 - Chapter 12: Routes */
 module.exports = function(app){
     app.get('/', function(req, res){
-        res.render('site/home')
+        //console.log(clientModel.all())
+        var clientList = clientModel.all()
+        res.render('site/home', {clients:clientList})
     })
     
     app.get('/contato', function(req, res){
