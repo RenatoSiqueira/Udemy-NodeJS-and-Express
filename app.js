9187ba0d6ac79c64d87d69fd419ef4fb27b0b106
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-13 12:53:24 -0300

    End of Lesson 6

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ var http = require('http')
 var server = http.createServer(function(req, res){
     var page = req.url
     console.log(page)
-    res.end(`
-        
-    `)
+    if(page == '/contato'){
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Introdução ao NodeJS</title>
+                </head>
+                <body>
+                    <h1>Página de Contato</h1>
+                </body>
+            </html>
+        `)
+    } else {
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Introdução ao NodeJS</title>
+                </head>
+                <body>
+                    <h1>Página de Home</h1>
+                </body>
+            </html>
+        `)        
+    }
 })
 
 console.log('localhost:8000')
