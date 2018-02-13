87a17cf9d2c949a5b84e7b491efd489390f01f28
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-13 12:49:36 -0300

    Returning pure html

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var http = require('http')
 var server = http.createServer(function(req, res){
     var page = req.url
     console.log(page)
-    res.end('URL: '+page)
+    res.end(`
+        
+    `)
 })
 
 console.log('localhost:8000')
