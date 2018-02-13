22582572d62c9c906d9259fee113b10bfdd9c803
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-13 13:14:38 -0300

    Lesson 1 - Chatper 8

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 /* First Command */
 //console.log('Hello Node!')
 
+//#########################
+
 /* Second Command */
-var http = require('http')
+/*var http = require('http')
 
 var server = http.createServer(function(req, res){
     var page = req.url
@@ -37,4 +39,44 @@ var server = http.createServer(function(req, res){
 })
 
 console.log('localhost:8000')
-server.listen(8000)
+server.listen(8000) */
+
+//#########################
+
+/* Lesson 1 - Chapter 8 */
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Introdução ao NodeJS</title>
+        </head>
+        <body>
+            <h1>Página de Home</h1>
+        </body>
+    </html>
+    `)
+})
+
+app.get('/contato', function(req, res){
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Introdução ao NodeJS</title>
+        </head>
+        <body>
+            <h1>Página de Contato</h1>
+        </body>
+    </html>
+    `)
+})
+
+app.listen(8000, function(){
+    console.log('localhost:8000')
+})
