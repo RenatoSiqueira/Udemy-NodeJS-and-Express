d256ad0bf60aa430c9373c85ab0816d28b22828a
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-02-14 23:55:17 -0300

    Lesson 1 - Chapter 15

--- app/models/clientModel.js
+++ app/models/clientModel.js
@@ -1,5 +1,19 @@
+/* Lesson 1 - Chapter 15 */
+var mysql = require('mysql')
+
+var conn = mysql.createConnection({
+    host: '192.168.99.100',
+    user: 'root',
+    password: '',
+    database: 'curso_node'
+})
+
+
 module.exports = function(){
     this.all = function(){
+        conn.query('select * from clientes', function(err, results){
+            console.log(results, err)
+        })
         return [
             {name:'Paulo', email:'paulo@email.com'}
         ]
