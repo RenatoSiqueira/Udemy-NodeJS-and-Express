af8b744cbddd12b91098475bfc510ced807f218f
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-01-11 16:54:57 -0300

    Aula 11

---
+++ Aula 11/app.js
@@ -0,0 +1,13 @@
+var app = require('./config/server')
+
+app.get('/contato', function(req, res){
+    res.render('site/contato')
+})
+
+app.get('/', function(req, res){
+    res.render('site/home')
+})
+
+app.listen('8000', function(){
+    console.log()
+})
